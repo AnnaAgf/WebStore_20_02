0b06082a8c7d3d4ffbe4db432b6af66222b76319
Anna <agan@sletat.ru>
2020-02-25 12:17:11 +0300

    fix Route

--- Services/WebStore.ServiceHosting/Controllers/ProductsApiController.cs
+++ Services/WebStore.ServiceHosting/Controllers/ProductsApiController.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain;
 using WebStore.Domain.DTO.Products;
 using WebStore.Domain.Entities;
 using WebStore.Interfaces.Services;
 
 namespace WebStore.ServiceHosting.Controllers
 {
-    [Route("WebAPI.Products)")]
+    [Route(WebAPI.Products)]
     [ApiController]
     public class ProductsApiController : ControllerBase, IProductData
     {
